a1777a71aecec916aebf218b4d8c36140bd272a8
Manas Mishra <manas.m22@gmail.com>
2021-05-03 22:06:43 +0530

    Just commiting changes

--- mm_sma_signals/README.md
+++
@@ -1,5 +0,0 @@
-# mm-sma-signals
-
-Python package to get trading signals using sma algorithm.
-
-It takes in a list of timestamps and price values and it will return whether you should buy/retain or sell/don't buy the stock.
--- mm_sma_signals/setup.py
+++ mm_sma_signals/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mm_sma_signals",
+    name="mm-sma-signals",
     author="Manas Mishra",
     author_email="manas.m22@gmail.com",
     description="Package to give trading signals from list of timestamps and prices",
-    long_description=open("README.md").read(),
+    long_description=open("../README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/manasm11/mm-sma-signals",
     version="0.0.0",
